429cdb94ef521f83472575a0c2d958823a6277e1
Vaibhav Kashyap <vaibhavkashyap230@gmail.com>
2019-07-28 18:40:07 +0530

    Add files via upload

---
+++ last occurance of a number in array O(log n).cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int fo(int a[],int n,int x)
+{
+	int s=0,e=n-1,r=-1;
+	while(s<=e)
+	{
+		int mid=(s+e)/2;
+		
+		if(a[mid]==x)
+		{
+			r=mid;
+			s=mid+1;
+		}
+		
+		else if(x>a[mid])
+		{
+			s=mid+1;
+		}
+		
+		else
+		e=mid-1;
+	}
+	return r;
+}
+int main()
+{
+	int a[12]={-12,-7,-5,3,9,9,9,15,49,55,99,105};
+	int x;
+	cout<<"Enter the number to find : ";
+	cin>>x;
+	int r=fo(a,sizeof(a)/sizeof(int),x);
+	if(r>-1)
+	cout<<"Present at "<<r<<endl;
+	else
+	cout<<"This number does not exist in the array."<<endl;
+	return(0);
+}
