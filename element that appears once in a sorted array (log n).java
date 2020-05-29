46524546303de7bf7422ae905456f25db5aa1499
Vaibhav Kashyap <vaibhavkashyap230@gmail.com>
2020-05-29 11:39:40 +0530

    Add files via upload

---
+++ element that appears once in a sorted array (log n).java
@@ -0,0 +1,86 @@
+// https://practice.geeksforgeeks.org/problems/find-the-element-that-appears-once-in-sorted-array/0
+
+import java.util.*;
+
+public class Solution
+{
+	public static void main(String args[])
+    {
+	    Scanner sc = new Scanner(System.in);
+	    int test = sc.nextInt();
+	    while(test-->0)
+	    {
+	        int n = sc.nextInt();
+	        int a[] = new int[n];
+	        for(int i=0;i<n;i++)
+	        {
+	            a[i] = sc.nextInt();
+	        }
+	        if(n==1)
+	        {
+	        	System.out.println(a[0]);
+	        }
+	        int s=0,e=n-1;
+	        while(s<=e)
+	        {
+	            int mid = (s+e)/2;
+	            boolean l,r;
+	            if(mid-1<0)
+	            {
+	            	l = true;
+	            }
+	            else if(mid-1>=0 && a[mid]!=a[mid-1])
+	            {
+	            	l = true;
+	            }
+	            else
+	            {
+	            	l = false;
+	            }
+	            
+	            if(mid+1>=n)
+	            {
+	            	r = true;
+	            }
+	            else if(mid+1<n && a[mid]!=a[mid+1])
+	            {
+	            	r = true;
+	            }
+	            else
+	            {
+	            	r = false;
+	            }
+	            
+	            if(l&r)
+	            {
+	            	System.out.println(a[mid]);
+	            	break;
+	            }
+	            
+	            if(mid%2==1)
+	            {
+	            	if(a[mid-1]==a[mid])
+	            	{
+	            		s = mid+1;
+	            	}
+	            	else
+	            	{
+	            		e = mid-1;
+	            	}
+	            }
+	            else
+	            {
+	            	if(a[mid-1]==a[mid])
+	            	{
+	            		e = mid-2;
+	            	}
+	            	else
+	            	{
+	            		s = mid+1;
+	            	}
+	            }
+	        }
+	    }
+	    sc.close();
+    }
+}
